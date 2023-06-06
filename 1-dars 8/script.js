8e1e9b0c7ff4f7a6a3178005d0ed5d96d231ae97
Abulxayrxon <132061576+Abulhayrhon@users.noreply.github.com>
2023-06-06 18:22:29 +0500

    Add files via upload

---
+++ 1-dars 8/script.js
@@ -0,0 +1,35 @@
+// function intFloor(num) {
+//     let result = num;
+//     if (num >= 0) {
+//       while (result >= 1) {
+//         result--;
+//       }
+//     } else {
+//       while (result <= -1) {
+//         result++;
+//       }
+//       result++;
+//     }
+//     return result;
+//   }
+//#################################################
+// function abs(x){
+//     if(x<0){
+//       return (-1)*x
+//     }
+//     else{
+//       return x
+//     }
+//   }
+//   console.log(abs(x))
+//################################################
+// function sqrt(x){
+//     if(x>=0){
+//       return x**(1/2)
+//     }
+//     else{
+//       return "Manfiy son kiritmang. Musbat son kiriting"
+//     }
+//   }
+//   console.log(sqrt(x))
+//#################################################
